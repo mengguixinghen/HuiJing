4c36ff12980d3cd464f3858c751e4d2182cc0ef5
星痕 <menggui@xinghendeMacBook-Air.local>
2023-05-20 02:38:40 +0800

    老王is stugying java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,21 @@
-public class Hello {
-      public static void main(String[] args) {
-               System.out.println("hello,world");
-        }
+//java快速入门，演示java开发步骤
+//1.public class Hello 表示Hello是一个类，是一个public公有的类
+//2.Hello{ }表示一个类的开始和结束
+//3.public static void main(String[] args)表示一个主方法，即我们程序的入口
+//4.main(){ }方法的开始和结束
+//5.System.out.println("hello,world")；表示输出"hello,world"到屏幕
+//6.；表示语句的结束
+/*
+public class Hello{
+
+	  //编写一个main方法
+	public static void main(String[] args){
+		System.out.println("hello,world");
+	}
+}
+*/
+public class Hello{
+	public static void main(String[] args){
+		System.out.println("老王 is studying java!");
+	}
 }
