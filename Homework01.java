9d2cea28a3801f752cd91e66e5e9b73fd511acd6
星痕 <menggui@xinghendeMacBook-Air.local>
2023-05-21 13:41:20 +0800

    第一章作业

---
+++ Homework01.java
@@ -0,0 +1,39 @@
+//1.编写hello,world程序
+/*
+public class Homework01{
+	public static void main(String[] args){
+		System.out.println("hello,world");
+	}
+}
+//2.将个人的基本信息（姓名、性别、籍贯、住址）打印到控制台输出。各条信息分别占一行。
+public class Homework01{
+	public static void main(String[] args){
+		//考察对转义字符使用
+		System.out.println("姓名\t性别\t籍贯\t住址\n小微\t女\t河北\t沧州");
+	}
+}
+//DK,JRE,JVM的关系
+1.JDK=JRE + java开发工具
+2.JRE=JVM(java虚拟机) + 核心类库
+//环境 变量path配置及其作用
+1.环境变量是为了在dos的任意目录可以去使用java和javac命令
+2.先配置JAVA_HOME = 指向jdk安装的主目录
+3.编辑path环境变量，增加%JAVA_HOME%\bin
+//java编写步骤
+1.编写java源代码并保存
+2.用javac编译得到对应的.class 字节码文件
+3.java运行，本质就是把.class 加载到jvm运行
+//java编写7个规范
+1.类，方法注释，使用javadoc的方式，即文档注释
+2.非javadoc注释，往往是对代码的说明(给程序的维护者看的)，说明如何修改及注意事项
+3.用tab整体将代码右移，用shift+tab整体左移
+4.运算符和 = 的两边，给空格，代码看上去清楚 int n = 1 + 4;
+5.源码文件使用 uft-8编码
+6.行宽字符不要超过 80
+7.代码编程风格有两种  次行风格，行尾风格(推荐)
+//总结初学者java易犯的错误
+1.编译或运行时，找不到文件 (文件名写错了或者路径整错了)
+2.主类名和文件名不一致，修改时保持一致即可
+3.缺少;
+4.拼写错误
+*/
